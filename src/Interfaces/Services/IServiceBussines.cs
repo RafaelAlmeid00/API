5f73d3bd9641d143b9af00075e5ca3ebd059decb
Luis Eduardo Spindola <eduspindola@live.com>
2024-05-22 16:00:07 -0300

    Fix: IServicesBussines

--- src/Interfaces/Services/IServiceBussines.cs
+++ src/Interfaces/Services/IServiceBussines.cs
@@ -1,7 +1,8 @@
 using Api.Domain;
+using Microsoft.CodeAnalysis.Operations;
 
 namespace Api.Interface;
 
-public interface IServiceBussines : IBaseService<Bussines>, IDeleteEditService<Bussines>, IAlterTypeService<Bussines>
+public interface IServiceBussines : IBaseService<Bussines>, IDeleteEditService<Bussines>, ILoginLogoutService<IBussinesLoginDTO>, IAlterTypeService<Bussines>, IDisableEnableService<Bussines>
 {
 }
